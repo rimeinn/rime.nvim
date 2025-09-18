34567253159cc0ef768a447723c317e7b6bb1438
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-09-18 18:52:48 +0800

    :art: Format ui.lua

--- lua/rime/ui.lua
+++ lua/rime/ui.lua
@@ -43,34 +43,33 @@ setmetatable(M.UI, {
 ---@param context table
 ---@return string[], integer
 function M.UI:draw(context)
-    local ui = self
     local preedit = context.composition.preedit or ""
     preedit = preedit:sub(1, context.composition.cursor_pos) ..
-        ui.cursor .. preedit:sub(context.composition.cursor_pos + 1)
+        self.cursor .. preedit:sub(context.composition.cursor_pos + 1)
     local candidates = context.menu.candidates
     local candidates_ = ""
-    local indices = ui.indices
+    local indices = self.indices
     for index, candidate in ipairs(candidates) do
         local text = indices[index] .. " " .. candidate.text
         if candidate.comment ~= nil then
             text = text .. " " .. candidate.comment
         end
         if (context.menu.highlighted_candidate_index + 1 == index) then
-            text = ui.left_sep .. text
+            text = self.left_sep .. text
         elseif (context.menu.highlighted_candidate_index + 2 == index) then
-            text = ui.right_sep .. text
+            text = self.right_sep .. text
         else
             text = " " .. text
         end
         candidates_ = candidates_ .. text
     end
     if (context.menu.num_candidates == context.menu.highlighted_candidate_index + 1) then
-        candidates_ = candidates_ .. ui.right_sep
+        candidates_ = candidates_ .. self.right_sep
     else
         candidates_ = candidates_ .. " "
     end
     local col = 0
-    local left = ui.left
+    local left = self.left
     if context.menu.page_no ~= 0 then
         local left_width = fs.strwidth(left)
         candidates_ = left .. candidates_
@@ -79,7 +78,7 @@ function M.UI:draw(context)
         col = col - left_width
     end
     if (context.menu.is_last_page == false and context.menu.num_candidates > 0) then
-        candidates_ = candidates_ .. ui.right
+        candidates_ = candidates_ .. self.right
     end
     return { preedit, candidates_ }, col
 end
